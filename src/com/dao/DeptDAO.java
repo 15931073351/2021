10043166ddd5889386047c003760085b091ea29d
15931073351 <82195366+15931073351@users.noreply.github.com>
2021-04-09 15:03:23 +0800

    Update DeptDAO.java

--- src/com/dao/DeptDAO.java
+++ src/com/dao/DeptDAO.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface DeptDAO {
+	//123
 	public List<Map<String,Object>> getAllDeptSelect();
 	public List<Map<String,Object>> getEmpByDid(int did);
 	public Map<String,Object> getEmpById(int eid);
